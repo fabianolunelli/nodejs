5dc991dd868f31216c225516c879c54349f22dc5
Fabiano Lunelli <fa.lunelli@gmail.com>
2020-04-11 09:47:59 -0300

    App implemented

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,57 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const rep = {
+    "id": uuid(),
+    "title": title,
+    "url": url,
+    "techs": techs,
+    "likes": 0
+  };
+  repositories.push(rep);
+  return response.json(rep);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repIdx = repositories.findIndex(r => r.id == id);
+  if (repIdx < 0) {
+    return response.status(400).json({ "error": "Repository not found." });
+  }
+  const rep = repositories[repIdx];
+  rep.title = title;
+  rep.url = url;
+  rep.techs = techs;
+  repositories[repIdx] = rep;
+  return response.json(rep);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repIdx = repositories.findIndex(r => r.id == id);
+  if (repIdx < 0) {
+    return response.status(400).json({ "error": "Repository not found." });
+  }
+  repositories.splice(repIdx);
+  return response.status(204).json({ "message": "Repository deleted!" });
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repIdx = repositories.findIndex(r => r.id == id);
+  if (repIdx < 0) {
+    return response.status(400).json({ "error": "Repository not found." });
+  }
+  const rep = repositories[repIdx];
+  rep.likes = rep.likes + 1;
+  repositories[repIdx] = rep;
+  return response.json(rep);
 });
 
 module.exports = app;
